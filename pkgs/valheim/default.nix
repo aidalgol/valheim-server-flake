93cf7ebff4062af11658dcd8dc2c573d3e6f66dc
Aidan Gauland <aidalgol@fastmail.net>
2023-04-14 10:39:55 +1200

    Correct fetchSteam arguments
    We were incorrectly using an older manifest that does not correspond to
    the package version string.

--- pkgs/valheim/default.nix
+++ pkgs/valheim/default.nix
@@ -15,8 +15,8 @@ stdenv.mkDerivation rec {
     inherit name;
     appId = "896660";
     depotId = "896661";
-    manifestId = "1096250207355556362";
-    hash = "sha256-oOHBv//sgpvowiXmongc49t6hjZt1vRQKl+or20oi+o=";
+    manifestId = "1755534777276869897";
+    hash = "sha256-fyctiui0Ee57gFIqJvAVOeOQItydx9Fop5F4nz6RpUQ=";
   };
 
   # Skip phases that don't apply to prebuilt binaries.

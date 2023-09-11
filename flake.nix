c50e6b58d98459d35f8bd7d6cc13cdaa8062f022
Aidan Gauland <aidalgol@fastmail.net>
2023-09-11 21:34:00 +1200

    Remove flake-utils
    Rationale: https://ayats.org/blog/no-flake-utils/

--- flake.nix
+++ flake.nix
@@ -3,10 +3,8 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
-    flake-utils.url = "github:numtide/flake-utils";
     steam-fetcher = {
       url = "github:nix-community/steam-fetcher";
-      inputs.flake-utils.follows = "flake-utils";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -14,55 +12,65 @@
   outputs = {
     self,
     nixpkgs,
-    flake-utils,
     steam-fetcher,
-  }:
-    with flake-utils.lib;
-      eachDefaultSystem (system: let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [steam-fetcher.overlays.default];
-        };
-
-        linters = with pkgs; [
-          alejandra
-          statix
-        ];
-      in {
-        devShells = {
-          default = pkgs.mkShell {
-            packages = with pkgs;
-              [
-                nil # Nix LS
-              ]
-              ++ linters;
-          };
-        };
-
-        checks = builtins.mapAttrs (name: pkgs.runCommandLocal name {nativeBuildInputs = linters;}) {
-          alejandra = "alejandra --check ${./.} > $out";
-          statix = "statix check ${./.} > $out";
-        };
-
-        formatter = pkgs.writeShellApplication {
-          name = "fmt";
-          runtimeInputs = linters;
-          text = ''
-            alejandra --quiet .
-            statix fix .
-          '';
-        };
-      })
-      // {
-        nixosModules = rec {
-          valheim = import ./nixos-modules/valheim.nix {inherit self steam-fetcher;};
-          default = valheim;
-        };
-        overlays.default = final: prev: {
-          valheim-server-unwrapped = final.callPackage ./pkgs/valheim-server {};
-          valheim-server = final.callPackage ./pkgs/valheim-server/fhsenv.nix {};
-          valheim-bepinex-pack = final.callPackage ./pkgs/bepinex-pack {};
-          fetchValheimThunderstoreMod = final.callPackage ./pkgs/build-support/fetch-thunderstore-mod {};
-        };
+  }: let
+    # The Steam Nix fetcher only supports x86_64 Linux.
+    supportedSystems = ["x86_64-linux"];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    pkgsFor = system:
+      import nixpkgs {
+        inherit system;
+        overlays = [steam-fetcher.overlays.default];
       };
+    lintersFor = system: let
+      pkgs = pkgsFor system;
+    in
+      with pkgs; [
+        alejandra
+        statix
+      ];
+  in {
+    devShells = forAllSystems (system: let
+      pkgs = pkgsFor system;
+    in {
+      default = pkgs.mkShell {
+        packages = with pkgs;
+          [
+            nil # Nix LS
+          ]
+          ++ lintersFor system;
+      };
+    });
+
+    checks = forAllSystems (system: let
+      pkgs = pkgsFor system;
+    in
+      builtins.mapAttrs (name: pkgs.runCommandLocal name {nativeBuildInputs = lintersFor system;}) {
+        alejandra = "alejandra --check ${./.} > $out";
+        statix = "statix check ${./.} > $out";
+      });
+
+    formatter = forAllSystems (system: let
+      pkgs = pkgsFor system;
+    in
+      pkgs.writeShellApplication {
+        name = "fmt";
+        runtimeInputs = lintersFor system;
+        text = ''
+          alejandra --quiet .
+          statix fix .
+        '';
+      });
+
+    nixosModules = rec {
+      valheim = import ./nixos-modules/valheim.nix {inherit self steam-fetcher;};
+      default = valheim;
+    };
+    overlays.default = final: prev: {
+      valheim-server-unwrapped = final.callPackage ./pkgs/valheim-server {};
+      valheim-server = final.callPackage ./pkgs/valheim-server/fhsenv.nix {};
+      valheim-bepinex-pack = final.callPackage ./pkgs/bepinex-pack {};
+      fetchValheimThunderstoreMod = final.callPackage ./pkgs/build-support/fetch-thunderstore-mod {};
+    };
+  };
 }
